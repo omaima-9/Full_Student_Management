aca64b6164d4aba52372b516017f4ee05e50c7da
omaima-9 <69462790+omaima-9@users.noreply.github.com>
2025-11-10 09:49:01 +0100

    Delete frontend/app directory

--- frontend/app/f1/(.)f2/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function InterceptedF2() {
-  return (
-    <h1 className="text-4xl font-bold tracking-tight bg-gradient-to-r from-amber-600 to-rose-500 bg-clip-text text-transparent">
-      (.) Intercepted F2 page
-    </h1>
-  );
-}
